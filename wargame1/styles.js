df1b30da5bd10a769e96ff288889a1d69b8083c2
GorpoOrko <47964672+gorpo@users.noreply.github.com>
2019-12-22 12:26:45 -0300

    Add files via upload

---
+++ wargame1/styles.js
@@ -0,0 +1,8 @@
+// www.100security.com.br
+var a;
+	a=prompt("Parabens! Você passou para o nível 04, Digite a Senha: ","");
+	if (a=="acesso") {
+		window.location="acesso.html";
+		} else {
+			alert("Tente novamente, pense fora da caixa lembra?");
+		}
